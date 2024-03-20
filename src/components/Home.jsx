ff2554149ef9d33b8a4b348836cf9323bf380358
Mohit Raj Parit <mohitrajparit@pop-os.localdomain>
2024-03-20 20:25:32 +0530

    adding animation lottie

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,22 +1,39 @@
 import React from 'react'
 import { HiArrowNarrowRight } from 'react-icons/hi';
 import { TypeAnimation } from 'react-type-animation';
-
+import Lottie from "lottie-react";
+import Animation from '../assets/Animation2.json'
 import { useEffect,useState } from 'react';
 import Navbar from './Navbar';
 const Home = () => {
   const [nav,setNav]=useState(true);
+  
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: Animation,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice'
+    }
+  };
+
+  
   return (
 <>
     <Navbar/>
-    <div name='home' className=' w-full h-screen bg-[#0a192f]'>
+    <div className='w-14' style={{ backgroundColor: '#0a192f' }}>
+    <Lottie animationData={Animation} loop={true} />
+</div>
+  
+<div className="grid grid-cols-2 gap-4 bg-[#0a192f]">
+    <div name='home' className='w-[110%] h-screen bg-[#0a192f]'>
       <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full '
       >
         <p className='text-green-600 text-3xl'>Hi my name is</p>
         <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>
           Mohit Raj Parit
         </h1>
-        <h2 className='text-1xl py-2 sm:text-2xl font-bold text-[#8892b0]'>
+        <h2 className='text-1xl py-2 sm:text-[1.35rem] font-bold text-[#8892b0]'>
           {true ? <TypeAnimation
           sequence={[
             'I \'m a Full Stack Developer',
@@ -26,12 +43,12 @@ const Home = () => {
           ]}
           wrapper="span"
           speed={50}
-          style={{ fontSize: '2em', display: 'inline-block' }}
+          style={{ fontSize: '1.5em', display: 'inline-block' }}
           repeat={Infinity}
-    />:<h2 className='text-[2em] inline-block'>I'm a Full Stack Developer</h2>}
+    />:<h2 className='text-[1.5em] inline-block'>I'm a Full Stack Developer</h2>}
        
         </h2>
-        <p className='text-[#8892b0] text-[1.25rem] py-4 max-w-[700px]'>
+        <p className='text-[#8892b0] text-[1.20rem] py-4 max-w-[700px]'>
         I’m a full-stack developer specializing in building (and occasionally
           designing) exceptional digital experiences. Currently, I’m focused on
           building responsive full-stack web applications.
@@ -45,6 +62,11 @@ const Home = () => {
 
           </button>
         </div>
+
+        </div>
+      </div>
+      <div>
+      <Lottie animationData={Animation} loop={true} width={2} />
       </div>
     </div>
     </>
@@ -52,6 +74,5 @@ const Home = () => {
 }
 
 export default Home
-
 
 
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
-
+import Navbar from './Navbar'
+import Aws from '../assets/aws.png'
 const Work = () => {
   return (
-    <div>Work</div>
+    <>
+    <Navbar/>
+
+    <div name='work' className=' w-full h-screen bg-[#0a192f]'>
+    <div className='max-w-[1000px] mx-auto p-4'>
+    <div className='mt-24'>
+    <p className='text-4xl font-bold inline border-b-4 border-green-600 text-gray-300'>Work</p>
+    <p className='py-4 text-lg text-gray-200'>These are some of my projects:</p>
+  </div>
+  <div className='w-full grid grid cols-1 sm:grid-cols-2 gap-4 text-center py-8 '>
+    <div className='w-[80%] h-30 shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+      <p>qwerty</p>
+      <img src={Aws} alt="Aws"/>
+      <button className='btn'>Live Demo</button>
+      <button className='btn'>Source Code</button>
+    </div>
+    <div className='w-[80%] h-20 shadow-md shadow-[#040c16] hover:scale-110 duration-500'></div>
+    <div className='w-[80%] h-20 shadow-md shadow-[#040c16] hover:scale-110 duration-500'></div>
+    <div className='w-[80%] h-20 shadow-md shadow-[#040c16] hover:scale-110 duration-500'></div>
+  </div>
+  </div>
+    </div>
+    </>
   )
 }
 
