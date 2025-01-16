08a33c943b05f29a5ffde7a43e62b5865b72f3bd
Mohit Raj Parit <97958965+mohitrajparit@users.noreply.github.com>
2025-01-16 19:28:19 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,39 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import About from './components/About';
 import Home from './components/Home';
 import Contact from './components/Contact';
 import Work from './components/Work';
 import Skills from './components/Skills';
-import Navbar from "./components/Navbar"
-import {Routes, Route} from 'react-router-dom';
-import Footer from './components/Footer'
-
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Design from './components/Design';
 function App() {
-
   return (
-    <>
-      {/* <Navbar/> */}
-      <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/about" element={<About/>}/>
-      <Route path="/contact" element={<Contact/>}/>
-      <Route path="/work" element={<Work/>}/>
-      <Route path="/skills" element={<Skills/>}/>
-      </Routes>
-      
-      {/* <Footer/> */}
-    </>
-      
-  )
+    <div>
+      <Navbar />
+
+      {/* Main content area */}
+      <main className="flex-grow bg-opacity-80 backdrop-blur-lg">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/work" element={<Work />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path='/design' element={<Design/>}/>
+          <Route path="*" element={<Home/>} />
+        </Routes>
+      </main>
+
+      {/* Footer is always visible */}
+      <Footer />
+    </div>
+    
+  
+  
+  
+  );
 }
 
-export default App
+export default App;
