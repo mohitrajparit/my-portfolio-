3d8960395dc9264c18e1417dfaceb870e58542a6
Mohit Raj Parit <97958965+mohitrajparit@users.noreply.github.com>
2025-01-20 13:19:11 +0530

    Update resume link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -88,7 +88,7 @@ const Navbar = () => {
     <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-110px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
       <a 
         className='flex justify-between items-center w-full text-gray-300' 
-        href="https://drive.google.com/file/d/1OmbycciWE2mEPPup6ZXJ_nJoTgsiSLTy/view?usp=sharing" 
+        href="https://drive.google.com/file/d/1mje1HtGahKJjbEISRp2VNIR8_8mwgGAb/view?usp=sharing" 
         target="_blank" 
         rel="noopener noreferrer"
       >
